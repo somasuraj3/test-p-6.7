74725ed61a20696027d6d3631ba4e01bcb1ae0c1
Suraj Soma <surajso@cybage.com>
2018-02-23 10:55:29 +0530

    Modifying properties type of otherMetrics and issueDetails as multi value

--- src/main/java/com/cybage/sonar/report/pdf/plugin/PDFReportPlugin.java
+++ src/main/java/com/cybage/sonar/report/pdf/plugin/PDFReportPlugin.java
@@ -10,8 +10,8 @@ import com.cybage.sonar.report.pdf.batch.PDFPostJob;
 @Properties({
 		@Property(key = PDFPostJob.SKIP_PDF_KEY, name = "Skip", description = "Skip generation of PDF report.", defaultValue = ""
 				+ PDFPostJob.SKIP_PDF_DEFAULT_VALUE, global = true, project = true, module = false, type = PropertyType.BOOLEAN),
-		@Property(key = PDFPostJob.OTHER_METRICS, name = "Other Metrics", description = "Comma separated metrics list that you would like to include explicitly in your PDF report.", defaultValue = "", global = true, project = true, module = false, type = PropertyType.STRING, multiValues = true),
-		@Property(key = PDFPostJob.TYPES_OF_ISSUE, name = "Issue Details", description = "Comma separated list of type of issues details that you would like to include explicitly in your PDF report. Allowed values are : ( BUG / CODE_SMELL / VULNERABILITY )", defaultValue = "NONE", global = true, project = true, module = false, type = PropertyType.STRING, multiValues = true) })
+		@Property(key = PDFPostJob.OTHER_METRICS, name = "Other Metrics", description = "Metrics list that you would like to include explicitly in your PDF report.", defaultValue = "", global = true, project = true, module = false, type = PropertyType.STRING, multiValues = true),
+		@Property(key = PDFPostJob.TYPES_OF_ISSUE, name = "Issue Details", description = "Type of issues details that you would like to include explicitly in your PDF report. Allowed values are : ( BUG / CODE_SMELL / VULNERABILITY )", defaultValue = "NONE", global = true, project = true, module = false, type = PropertyType.STRING, multiValues = true) })
 
 public class PDFReportPlugin implements Plugin {
 
